57b34bd603ca501c3cd35bd859d9d1200c0739ac
aaron-esc-13 <156145840+aaron-esc-13@users.noreply.github.com>
2024-01-12 11:11:51 -0800

    part 2 complete and part 3 new

--- Lab0/src/square.py
+++ Lab0/src/square.py
@@ -4,6 +4,6 @@ pinC0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP)
 
 while True:
     pinC0.value(0)
-    utime.sleep(2)
+    utime.sleep(5)
     pinC0.value(1)
-    utime.sleep(2)
+    utime.sleep(5)
