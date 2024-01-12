3952532da16766c0f20f2c86a035e7ca68ffea54
aaron-esc-13 <156145840+aaron-esc-13@users.noreply.github.com>
2024-01-12 11:09:24 -0800

    Update square.py

--- Lab0/src/square.py
+++ Lab0/src/square.py
@@ -1,4 +1,9 @@
-pinA5 = pyb.Pin(pyb.Pin.board.PA5, pyb.Pin.OUT_PP)
-tim2 = pyb.Timer(2, freq=0.1)
-ch2 = tim2.channel(2, pyb.Timer.PWM, pin=pinA5)
-ch2.pulse_width_percent(50)
+import utime
+
+pinC0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP)
+
+while True:
+    pinC0.value(0)
+    utime.sleep(2)
+    pinC0.value(1)
+    utime.sleep(2)
