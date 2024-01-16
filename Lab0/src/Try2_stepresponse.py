eae76fcc384cdf6ff0ee3971d53a2f73ed41a92b
Karen48468 <156145633+Karen48468@users.noreply.github.com>
2024-01-16 13:40:33 -0800

    it work

--- Lab0/src/Try2_stepresponse.py
+++ Lab0/src/Try2_stepresponse.py
@@ -4,26 +4,19 @@ import pyb
 import cqueue
 
 pinC0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP, value=0)
-pinB0 = pyb.Pin(pyb.Pin.board.PB0, pyb.Pin.OUT_PP)
-adcB0 = pyb.ADC (pinB0)
+pinB0 = pyb.Pin(pyb.Pin.board.PB0, pyb.Pin.OUT_PP, value=0)
+adcB0 = pyb.ADC(pinB0)
 
 micropython.alloc_emergency_exception_buf(100)
 
-Queue_Size = 200
+Queue_Size = 500
 volts = cqueue.IntQueue(Queue_Size)
-
-# Q: How to call interrupt in function
-# # Input loop
-# while not volts.full():
-#     if pinC0.high():
-#        pinC0.low()   # when wave goes high
-#     
-#     else:
-#        pinC0.high()   # when wave goes low
 
 # Measure and store voltage output from B0
 timer_data = pyb.Timer(1, freq=1000)
 timer_input = pyb.Timer(1, freq=200)             # Timer 1 running at 200 Hz for a 10ms function period
+time = list(range(0, 1991, 10))
+adc_conversion = []
 
 def timer_interrupt(timer_data):
     output_voltage = volts.put(adcB0.read())
@@ -37,8 +30,17 @@ def step_response():
     while not volts.full():
         pass
     pinC0.low()
-    print(volts)
     
+    i = 0
+    while i < volts.available():
+        adc_conversion.append(3.3*volts.get()/4096)
+        i = i+1
+        
+    #print(volts) # convert later to volts
+    
+    #print(f"{time}, {adc_conversion}")
+    print(f"{time}")
+    print(f"{adc_conversion}")
     
 step_response()
 
